388987ec34ab9e6e9114d264b7c63d9a8d335b14
Sukhpreet Singh <sukhpreet8661@gmail.com>
2025-01-30 05:47:48 +0530

    Add files via upload

---
+++ Cursor/JS/script.js
@@ -0,0 +1,17 @@
+var main = document.querySelector("#main");
+var cursor = document.querySelector(".cursor");
+var body = document.querySelector("body");
+
+main.addEventListener("mousemove", function(e){
+    cursor.style.left = e.x + "px"
+    cursor.style.top = e.y + "px"
+    cursor.style.opacity= "1";
+});
+
+body.addEventListener("mouseenter", function() {
+    cursor.style.display = "block";
+});
+
+body.addEventListener("mouseleave", function() {
+    cursor.style.display = "none";
+});
